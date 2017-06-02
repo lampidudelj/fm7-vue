5fb1691c48d2d2a478f439c795618401e7e0686b
Aleks Obrazcova <aobrazcova@gmail.com>
2017-06-02 14:23:52 +0100

    added tabbar

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# fm7-vue
-Follow intsallation guidde at https://framework7.io/vue/installation.html
+## Build Setup
+
+``` bash
+# install cordova
+npm i cordova -g
+
+# run your code (optional argument stands for live-reload)
+cordova (run|emulate) [android|ios|browser] [-- --lr]
+
+# build for production
+cordova build --release
+```
+
+## Project Structure
+
+src - Source files are here. Mostly your target is this directory.
+src/assets/static - Your static assets like images, css, fonts, json, js, sass is here.
+src/assets/static/css - put custom app CSS styles here. Don't forget to import them in main.js
+src/assets/vue/pages - app .vue pages
+src/assets/vue/components - folder with custom .vue components
+src/main.js - main app file where you include/import all required libs and init app
+src/routes.js - app routes
+src/main.vue - main app structure/component
+
+More details - https://framework7.io/vue/templates.html
--- src/main2.vue
+++ src/main2.vue
@@ -10,24 +10,40 @@
 			<f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
 				<!-- Navbar -->
 				<f7-navbar>
-					<f7-nav-center sliding>Framework7</f7-nav-center>
+					<f7-nav-center sliding>EIS Advisor</f7-nav-center>
 				</f7-navbar>
 				<!-- Pages -->
 				<f7-pages>
 					<f7-page>
-						<f7-block-title>Welcome to my App</f7-block-title>
-						<f7-block inner>
-							<p>Duis sed erat ac eros ultrices pharetra id ut tellus. Praesent rhoncus enim ornare ipsum aliquet ultricies. Pellentesque sodales erat quis elementum sagittis.</p>
-						</f7-block>
-						<f7-block-title>Navigation</f7-block-title>
-						<f7-list>
-							<f7-list-item link="/about/" title="About"></f7-list-item>
-							<f7-list-item link="/form/" title="Form"></f7-list-item>
-							<f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
-						</f7-list>
-						<f7-toolbar tabbar>
-                          <f7-link tab-link="/about/">Tab 1</f7-link>
-                          <f7-link tab-link="/form/">Tab 2</f7-link>
+                        <f7-tabs>
+                          <f7-tab id="tab1" active>						
+                              <f7-block-title>Welcome to my App</f7-block-title>
+                                <f7-block inner>
+                                    <p>Duis sed erat ac eros ultrices pharetra id ut tellus. Praesent rhoncus enim ornare ipsum aliquet ultricies. Pellentesque sodales erat quis elementum sagittis.</p>
+                                </f7-block>
+                                <f7-block-title>Navigation</f7-block-title>
+                                <f7-list>
+                                    <f7-list-item link="/about/" title="About"></f7-list-item>
+                                    <f7-list-item link="/form/" title="Form"></f7-list-item>
+                                    <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
+                                </f7-list>
+                          </f7-tab>
+                          <f7-tab id="tab2">
+                              <f7-block-title>Search</f7-block-title>
+                          </f7-tab>
+                          <f7-tab id="tab3">
+                              <f7-block-title>Settings</f7-block-title>
+                          </f7-tab>
+                          <f7-tab id="tab4">
+                              <f7-block-title>About</f7-block-title>
+                          </f7-tab>
+                        </f7-tabs>
+
+                        <f7-toolbar tabbar>
+                          <f7-link tab-link="#tab1" class="active">Home</f7-link>
+                          <f7-link tab-link="#tab2">Search</f7-link>
+                          <f7-link tab-link="#tab3">Settings</f7-link>
+                          <f7-link tab-link="#tab4">About</f7-link>
                         </f7-toolbar>
 					</f7-page>
 				</f7-pages>
@@ -37,5 +53,6 @@
 </template>
 
 <script>
-	export default {}
+    export default {}
+
 </script>
